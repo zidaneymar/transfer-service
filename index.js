5e57c7dcbecccd130d16b8fd8e7e8026c0ab600c
Qi Kang <qi@duolingo.com>
2022-09-05 13:03:50 +0800

    Fix

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get("/api/wx_openid", async (req, res) => {
 const port = process.env.PORT || 80;
 
 async function bootstrap() {
-  // await initDB();
+  await initDB();
   app.listen(port, () => {
     console.log("启动成功", port);
   });
