c093ba9a4f12a565680d5a88894628ed7d8a0065
Qi Kang <qi@duolingo.com>
2022-09-05 11:38:50 +0800

    Add logic

--- index.js
+++ index.js
@@ -6,23 +6,28 @@ const { init: initDB, Counter } = require("./db");
 
 const logger = morgan("tiny");
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(logger);
-app.use('/', createProxyMiddleware({
-  target: 'http://eaa3-5-161-87-7.ngrok.io',
-  changeOrigin: true,
-}))
 
 
-// // 首页
-// app.get("/", async (req, res) => {
-//   res.sendFile(path.join(__dirname, "index.html"));
-// });
+
+// 首页
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.post("/", async (req, res) => {
+  const prompt = req.body.prompt;
+  const response = await axios.post("https://eaa3-5-161-87-7.ngrok.io", {
+    prompt: prompt
+  });
+
+  res.send(response.data);
+})
 
 // 更新计数
 app.post("/api/count", async (req, res) => {
